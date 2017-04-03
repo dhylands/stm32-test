302343a310f70ec1e22cd0c697e2945de28ecef7
Dave Hylands <dhylands@gmail.com>
2017-04-03 13:38:07 -0700

    Added VCC-GND-F407 board

---
+++ board-VCC-GND-F407/config.h
@@ -0,0 +1,10 @@
+#include <stm32f4xx_hal_conf.h>
+#include <stm32f4xx.h>
+#include <stm32f4xx_hal.h>
+
+#define LED1_PORT   GPIOB
+#define LED1_PIN    GPIO_PIN_9
+
+// PA14 is the SWCLK pin
+#define SW_PORT     GPIOA
+#define SW_PIN      GPIO_PIN_14
